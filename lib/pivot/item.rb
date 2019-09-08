77bd3536652c862cdd4aed2709a23a592380302a
Jimbo Cortes <jimbo.cortes@gmail.com>
2019-09-08 22:22:56 +0800

    Pass item test

--- lib/pivot/item.rb
+++ lib/pivot/item.rb
@@ -1,5 +1,30 @@
 module Pivot
   class Item
-    # Code here!
+    attr_reader :name, :assignee, :points
+
+    def initialize(attributes={})
+      @name = attributes[:name]
+      @assignee = attributes[:assignee]
+      @points = attributes[:points]
+    end
+
+    def +(item)
+      self.points + item.points
+    end
+
+    def project_code
+      regex_res = self.name.match(/\w+/)
+      regex_res ? regex_res[0] : nil
+    end
+
+    def valid?
+       valid_codes.include? project_code
+    end
+
+    private
+    
+    def valid_codes
+      %w(EREC AZR)
+    end
   end
 end
